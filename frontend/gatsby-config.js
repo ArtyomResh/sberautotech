8882e7eb63d2fc0039f92c84c047a9ecfbfa0ca2
Alexander Syrtsov <aasyrtsov@sberbank.ru>
2021-04-13 13:46:28 +0300

    FE-357 Remove sharp

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
         allExtensions: true,
       },
     },
+    "gatsby-plugin-postcss",
     "gatsby-plugin-react-helmet",
     {
       resolve: `gatsby-source-filesystem`,
@@ -28,22 +29,6 @@ module.exports = {
         singleTypes: [`homepage`, `global`, 'self-driving-car', 'career'],
         queryLimit: 1000,
       },
-    },
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-sharp",
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: "gatsby-starter-default",
-        short_name: "starter",
-        start_url: "/",
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/favicon.png`,
-      },
-    },
-    "gatsby-plugin-offline",
-    "gatsby-plugin-postcss"
+    }
   ],
 };
