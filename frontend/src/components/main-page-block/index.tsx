a3ef3659bbec16846ad4d6263028dfa5e57053bd
Artem Guslov <ASGuslov@sberbank.ru>
2021-04-15 22:33:20 +0300

    FE-388: Главный лендинг - доделать верстку -- тексты и стили

--- frontend/src/components/main-page-block/index.tsx
+++ frontend/src/components/main-page-block/index.tsx
@@ -2,19 +2,33 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 import { useClassnames } from '../../hooks/use-classnames';
-import StoryCard from '../story-card';
+import StoryCard, { ICard } from '../story-card';
 
 import style from './index.css';
 
-const MainPageBlock = ({data}) => {
+interface IData {
+    position: string,
+    background: {
+        publicURL: string
+    },
+    link?: {
+        to: string,
+        text: string
+    },
+    text: string,
+    cards: Array<ICard>
+}
+
+const MainPageBlock = ({ data }: { data: IData }) => {
     const cn = useClassnames(style);
+
     return (
         <div className={cn('block__wrapper')} id={data.position}>
-            <img src={data.background.publicURL} className={cn('block__image')} />
+            <img src={data.background.publicURL} className={cn('block__image')} alt="" />
             {
-                (data.link) ? (
+                data.link ? (
                     <Link to={data.link.to} className={cn('block__link')}>
-                        <button>{data.link.text}</button>
+                        {data.link.text}
                     </Link>
                 ) : null
             }
@@ -22,12 +36,12 @@ const MainPageBlock = ({data}) => {
                 <span className={cn('block__text')}>{data.text}</span>
                 {
                     data.cards.length ? data.cards.map((card) => (
-                        <StoryCard key={card.id} data={card} />
+                        <StoryCard key={card.id} card={card} />
                     )) : null
                 }
-            </div>  
+            </div>
         </div>
-    )
+    );
 };
 
 export default MainPageBlock;
--- frontend/src/components/story-card/index.tsx
+++ frontend/src/components/story-card/index.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import { Link } from 'gatsby';
+
 import { useClassnames } from '../../hooks/use-classnames';
 
 import style from './index.css';
 
-const StoryCard = ({ data }) => {
+export interface ICard {
+    id: string,
+    image: {
+        publicURL: string
+    },
+    text: string
+}
+
+const StoryCard = ({ card }: { card: ICard }) => {
     const cn = useClassnames(style);
+
     return (
         <div className={cn('story__wrapper')}>
-            <img src={data.image.publicURL} className={cn('story__image')} />
-            <span className={cn('story__text')}>{data.text}</span>
+            <img src={card.image.publicURL} className={cn('story__image')} alt="" />
+            <span className={cn('story__text')}>{card.text}</span>
         </div>
-    )
+    );
 };
 
 export default StoryCard;
