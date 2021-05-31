fef7e3c1b0e2f52643b9a4384a356cca3a44b61c
Boris Sapak <bsesapak@sberbank.ru>
2021-05-31 13:14:55 +0300

    fix vacancies 3 | FE-597

--- frontend/src/components/vacancies-list/index.tsx
+++ frontend/src/components/vacancies-list/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { useForm, FormProvider } from 'react-hook-form';
 
 import { useClassnames } from '../../hooks/use-classnames';
+import Button from '../button';
+import Select from '../select';
 
 import style from './index.css';
 import VacanciesItem from './item';
-import Button from '../button';
 
 interface IProps {
     data: IVacanciesList
@@ -27,20 +29,44 @@ export interface IVacanciesListItem {
     expect: Array<string>
 }
 
+const options = [
+    { value: 'Product', label: 'Product' },
+    { value: 'Development', label: 'Development' },
+    { value: 'HR', label: 'HR' },
+    { value: 'Design', label: 'Design' }
+];
+
 const VacanciesList: React.FC<IProps> = ({ data }) => {
     const cn = useClassnames(style);
 
+    const context = useForm({
+        mode            : 'onSubmit',
+        reValidateMode  : 'onChange',
+        shouldFocusError: true,
+        defaultValues   : {}
+    });
+
     return (
-        <div className={cn('vacancies__wrap')}>
-            <div className={cn('vacancies__list')}>
-                {data.vacancies_list.map((item, i) => (
-                    <VacanciesItem data={item} company={data.company} key={`vacancy-item-${i}`} />
-                ))}
-            </div>
-            <div className={cn('vacancies__btn-more')}>
-                <Button label="Загрузить еще вакансии" />
-            </div>
-        </div>
+        <FormProvider {...context}>
+            <form onSubmit={context.handleSubmit(onSubmit)}>
+                <div className={cn('vacancies__wrap')}>
+                    <div className={cn('vacancies__filters-wrap')}>
+                        <Select name="" placeholder="Функциональная сфера" options={options} />
+                        <Select name="" placeholder="Регион" options={options} />
+                        <Select name="" placeholder="График" options={options} />
+                    </div>
+                    <div className={cn('vacancies__total-count')}>99 вакансий найдено</div>
+                    <div className={cn('vacancies__list')}>
+                        {data.vacancies_list.map((item, i) => (
+                            <VacanciesItem data={item} company={data.company} key={`vacancy-item-${i}`} />
+                        ))}
+                    </div>
+                    <div className={cn('vacancies__btn-more')}>
+                        <Button label="Загрузить еще вакансии" />
+                    </div>
+                </div>
+            </form>
+        </FormProvider>
     );
 };
 
