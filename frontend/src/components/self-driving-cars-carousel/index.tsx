1628796aa639d518bb2080dd0e4200d70fdc64b1
Mikhail <mmborisov@sberbank.ru>
2021-05-31 16:04:17 +0300

    added

--- frontend/src/components/self-driving-cars-carousel/index.tsx
+++ frontend/src/components/self-driving-cars-carousel/index.tsx
@@ -1,5 +1,6 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react';
 import Swiper from 'swiper';
+// import GetCursorPosition from 'cursor-position';
 
 import { useClassnames } from '../../hooks/use-classnames';
 import useFormattedText from '../../hooks/use-formatted-text';
@@ -34,6 +35,8 @@ const Carousel: React.FC<IProps> = ({ data }) => {
     const cn = useClassnames(style);
     const $container = useRef<HTMLDivElement>(null);
     const [cursorPosition, setCursorPosition] = useState(ECursorPosition.left);
+    const [cursorCoordinates, setCursorCoordinates] = useState({ currentPositionX: 0, currentPositionY: 0, scroll: 0 });
+    // const [scroll, setScroll] = useState(0);
     const [swiper, setSwiper] = useState<any>(null);
 
     const header = useFormattedText(data.header);
@@ -49,8 +52,33 @@ const Carousel: React.FC<IProps> = ({ data }) => {
 
     const observeCursor = useCallback((e) => {
         const containerWidth = e.target.clientWidth;
-        const currentPosition = e.clientX;
-        const newCursorPosition = containerWidth / 2 >= currentPosition ? ECursorPosition.right : ECursorPosition.left;
+        // const currentPositionX = e.clientX;
+        // const currentPositionY = e.clientY;
+
+        // console.log(e.pageYOffset);
+
+
+        // console.log('scroll', scroll, 'cursor', currentPositionY);
+
+
+        // currentPositionY = Number(currentPositionY);
+        // currentPositionY = Number(currentPositionY) + (Number(currentPositionY) - scroll);
+        // const cursorEl = document.querySelector('.carousel__cursor');
+        // console.log(e);
+
+
+        // console.log(cursorEl.style.top = `${currentPositionY}px`);
+        // console.log(cursorEl.style.left = `${currentPositionX}px`);
+
+        // cursorEl.style.top = currentPositionX;
+
+
+        setCursorCoordinates({ currentPositionX: e.clientX, currentPositionY: e.clientY });
+
+        // console.log(cursorCoordinates);
+
+
+        const newCursorPosition = containerWidth / 2 >= cursorCoordinates.currentPositionX ? ECursorPosition.right : ECursorPosition.left;
 
         if(newCursorPosition !== cursorPosition) {
             setCursorPosition(newCursorPosition);
@@ -74,6 +102,40 @@ const Carousel: React.FC<IProps> = ({ data }) => {
         }
     }, []);
 
+    useEffect(() => {
+        // document.addEventListener('mousemove', (e) => {
+        //     // setScroll(window.pageYOffset);
+        //     console.log(e.clientX, e.clientY);
+
+
+        //     setCursorCoordinates({ ...cursorCoordinates, currentPositionX: e.clientX, currentPositionY: e.clientY });
+        // });
+
+        document.addEventListener('scroll', () => {
+            // console.log(window.pageYOffset);
+            // setScroll(window.pageYOffset);
+            setCursorCoordinates(cursorCoordinates.scroll: window.pageYOffset);
+            console.log(cursorCoordinates.scroll);
+        });
+
+
+        // console.log(scroll, cursorCoordinates);
+
+
+        // console.log(container);
+
+        // body.addEventListener('mousemove', () => {
+
+        // });
+        // setScroll(window.pageYOffset);
+
+        // document.body.addEventListener('scroll', (e) => {
+        //     console.log(e.clientY);
+        // });
+
+        // console.log('scroll', scroll, 'cursor', cursorCoordinates.currentPositionY);
+    }, [cursorCoordinates.scroll]);
+
     return (
         <div
             className={cn('carousel-container', 'carousel', `carousel_${cursorPosition}`)}
@@ -81,16 +143,7 @@ const Carousel: React.FC<IProps> = ({ data }) => {
             onMouseMove={observeCursor}
             onClick={onClick}
         >
-            {header && (
-                <p
-                    className={cn('carousel__header',
-                        {
-                            [`carousel__header_${data.header_position?.replace('_', '-')}`]: data.header_position
-                        }
-                    )}
-                    dangerouslySetInnerHTML={{ __html: header }}
-                />
-            )}
+            <div className={cn('carousel__cursor')} style={{ top: cursorCoordinates.currentPositionY, left: cursorCoordinates.currentPositionX }}></div>
             {text && (
                 <p
                     className={cn('carousel__text',
