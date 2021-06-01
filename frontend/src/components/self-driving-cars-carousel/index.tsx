982aac2c4f9aec544cfd68c3aa936ce9f115b88d
Mikhail <mmborisov@sberbank.ru>
2021-06-01 19:08:43 +0300

    added: spinner

--- frontend/src/components/self-driving-cars-carousel/index.tsx
+++ frontend/src/components/self-driving-cars-carousel/index.tsx
@@ -35,24 +35,25 @@ enum ECursorPosition {
 const Carousel: React.FC<IProps> = ({ data }) => {
     const cn = useClassnames(style);
     const $container = useRef<HTMLDivElement>(null);
-    const [cursorPosition, setCursorPosition] = useState(ECursorPosition.none);
+    const [cursorPosition, setCursorPosition] = useState({ prevPos: '', actualPos: ECursorPosition.none });
+    const [spinner, setSpinner] = useState(true);
     const [cursorCoordinates, setCursorCoordinates] = useState({ currentPositionX: 0, currentPositionY: 0 });
     const [swiper, setSwiper] = useState<any>(null);
 
-    const header = useFormattedText(data.header);
+    // const header = useFormattedText(data.header);
     const text = useFormattedText(data.text);
 
-    const hoverListener = (e) => {
-        setCursorPosition(ECursorPosition.none);
+    const hoverListener = () => {
+        setCursorPosition({ prevPos: cursorPosition.actualPos, actualPos: ECursorPosition.none });
     };
 
     const onClick = useCallback(() => {
-        if(cursorPosition === ECursorPosition.right) {
+        if(cursorPosition.actualPos === ECursorPosition.right) {
             swiper?.slidePrev();
-        } else if(cursorPosition === ECursorPosition.left) {
+        } else if(cursorPosition.actualPos === ECursorPosition.left) {
             swiper?.slideNext();
         }
-    }, [cursorPosition, swiper]);
+    }, [cursorPosition.actualPos, swiper]);
 
     const observeCursor = useCallback((e) => {
         const containerWidth = e.target?.closest('.carousel-container')?.clientWidth;
@@ -66,7 +67,7 @@ const Carousel: React.FC<IProps> = ({ data }) => {
         const { top, bottom } = e.target?.closest('.carousel-container')?.getBoundingClientRect();
 
         if(currentPositionY < top || currentPositionY > bottom) {
-            setCursorPosition(ECursorPosition.none);
+            setCursorPosition({ prevPos: cursorPosition.actualPos, actualPos: ECursorPosition.none });
 
             return;
         }
@@ -75,10 +76,10 @@ const Carousel: React.FC<IProps> = ({ data }) => {
 
         const newCursorPosition = containerWidth / 2 >= cursorCoordinates.currentPositionX ? ECursorPosition.left : ECursorPosition.right;
 
-        if(newCursorPosition !== cursorPosition) {
-            setCursorPosition(newCursorPosition);
+        if(newCursorPosition !== cursorPosition.actualPos) {
+            setCursorPosition({ prevPos: cursorPosition.actualPos, actualPos: newCursorPosition });
         }
-    }, [cursorPosition, cursorCoordinates]);
+    }, [cursorPosition.actualPos, cursorCoordinates]);
 
     useEffect(() => {
         if($container.current) {
@@ -105,26 +106,38 @@ const Carousel: React.FC<IProps> = ({ data }) => {
         };
     }, []);
 
-    // useEffect(() => {
-    //     const cursor = document.querySelector('.carousel__cursor');
-    //     const rotator = (direction) => {
-    //         let deg;
+    useEffect(() => {
+        const cursor = document.querySelector('.carousel__cursor');
+        const rotator = () => {
+            let i = 0;
+            const howManyTimes = 180;
+            console.log(spinner, '<<<<<<<<<<<<');
+            (function f() {
+                cursor.style.transform = `rotate(${i}deg)`;
+                console.log(i);
+                i++;
 
-    //         if(direction === 'left') {
+                if(i < howManyTimes) {
+                    setTimeout(f, 0.05);
+                    setSpinner(false);
+                }
+            })();
+            i = 0;
+            console.log(spinner, i, '>>>>>>>>>>>>>>>');
+        };
 
-    //         }
-    //     };
-
-    //     if(cursorPosition === 'left') {
-    //         rotator(cursorPosition);
-    //     } else if(cursorPosition === 'right') {
-    //         // rotator(cursorPosition);
-    //     }
-    // }, [cursorPosition]);
+        if(cursorPosition.prevPos === 'left' && cursorPosition.actualPos === 'right' && spinner) {
+            rotator();
+        } else if(cursorPosition.prevPos === 'right' && cursorPosition.actualPos === 'left' && spinner) {
+            rotator();
+        }
+    }, [cursorPosition]);
 
     const elCursor = useMemo(() => {
-        return <div className={cn('carousel__cursor', `carousel__cursor_${cursorPosition}`)} style={{ top: cursorCoordinates.currentPositionY, left: cursorCoordinates.currentPositionX }}></div>;
-    }, [cursorPosition, cursorCoordinates]);
+        return <div className={cn('carousel__cursor', `carousel__cursor_${cursorPosition.actualPos}`)} style={{ top: cursorCoordinates.currentPositionY, left: cursorCoordinates.currentPositionX }}></div>;
+    }, [cursorPosition.actualPos, cursorCoordinates]);
+
+    // console.log(cursorPosition);
 
     return (
         <div
@@ -132,7 +145,7 @@ const Carousel: React.FC<IProps> = ({ data }) => {
             ref={$container}
             onMouseMove={observeCursor}
             onMouseLeave={() => {
-                setCursorPosition(ECursorPosition.none);
+                setCursorPosition({ prevPos: cursorPosition.actualPos, actualPos: ECursorPosition.none });
             }}
             onClick={onClick}
         >
