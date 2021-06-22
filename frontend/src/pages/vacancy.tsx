a0dba269c5225e6848e8ef6ed0b0d47fafb9ef94
Mikhail <mmborisov@sberbank.ru>
2021-06-22 16:54:36 +0300

    added: vacancy page

---
+++ frontend/src/pages/vacancy.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const vacancy = () => {
+    return (
+        <Layout theme={{ mode: 'dark', logoColor: '#040A0A' }} pageNumber={3}>
+            TestString
+        </Layout>
+    )
+}
+
+export default vacancy
