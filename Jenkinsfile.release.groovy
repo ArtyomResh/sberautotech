041ff18e8d605d0e140a4d4f0ce98f3cf82a69b6
Evteev Andrey <evteev.a.al@sberbank.ru>
2021-06-27 14:54:51 +0300

    Update release job

--- Jenkinsfile.release.groovy
+++ Jenkinsfile.release.groovy
@@ -5,101 +5,89 @@ timestamps {
         wrap([$class: 'AnsiColorBuildWrapper', colorMapName: "xterm"]) {
             cleanWs()
 
-            gitlabBuilds(builds: ['Prepare', 'Clone source', 'Check image', 'Deploy']) {
-                gitlabCommitStatus('Prepare') {
-                    stage('Prepare') {
-                        if (params.version == '') {
-                            error('Version must be specified')
-                        }
-
-                        configFileProvider([configFile(fileId: params.config, variable: 'configFile')]) {
-                            config = readJSON file: configFile
-                        }
-
-                        repository = [
-                            'address': scm.getUserRemoteConfigs()[0].getUrl(),
-                            'source_branch': scm.getBranches()[0].toString(),
-                            'target_branch': scm.getBranches()[0].toString(),
-                            'credentials': config.gitlab.ssh_credentials
-                        ]
-
-                        repo_name = repository.address.split('/')[4].replace('.git', '')
-                        app_name = repo_name.replaceAll("_|\\.", "-")
-                        manager.addBadge('orange-square.png', "Version #${params.version}", params.version)
-                        info = "*${repo_name}*: Deploy on PROD\n\nVersion: *${params.version}*\nJob URL: ${env.BUILD_URL}\nStatus: "
-                    }
+            stage('Prepare') {
+                if (params.version == '') {
+                    error('Version must be specified')
                 }
 
-                try {
-                    gitlabCommitStatus('Clone source') {
-                        stage('Clone source') {
-                            try {
-                                gitUtils.checkoutRepository(repository, false)
-                            } catch (checkoutError) {
-                                log.error("Checkout Failed")
-                                info += ":x: checkout faled"
-                                throw checkoutError
-                            }
-
-                            log.info("Checked out branch: ${params.version}")
-                        }
-                    }
-
-                    gitlabCommitStatus('Check image') {
-                        stage('Check image') {
-                            image = "${config.docker_registry.host}/${config.docker_registry.dir}/${repo_name}/${params.package}:${version}"
-                            log.info("Pull image ${image}")
-
-                            docker.withRegistry("https://${config.docker_registry.host}", config.docker_registry.credentials) {
-                                try {
-                                    sh("docker pull ${image}")
-                                } catch (pullError) {
-                                    log.error("Failed to pull release image: ${pullError}")
-                                    info += ":warning:"
-                                    throw pullError
-                                }
-                            }
-                        }
-                    }
-
-                    gitlabCommitStatus('Deploy') {
-                        stage('Deploy') {
-                            stand = 'infra'
-                            log.info("Deploy ${image} on PROD")
-
-                            dir("${WORKSPACE}/${params.package}/devops/k8s") {
-                                try {
-                                    withCredentials([file(credentialsId: config.k8s[stand].credentials, variable: 'k8sConfig')]) {
-                                        ansiblePlaybook(
-                                            colorized: true,
-                                            playbook: 'playbook.yml',
-                                            extraVars: [
-                                                k8s_config: k8sConfig,
-                                                app_name: app_name,
-                                                package_name: params.package,
-                                                app_version: version,
-                                                image: image,
-                                                stand: stand
-                                            ]
-                                        )
-                                    }
-                                } catch (deployError) {
-                                    log.error("Failed to deploy image: ${deployError}")
-                                    info += ":warning:"
-                                    throw deployError
-                                }
-                            }
-                        }
-                    }
-                } catch (Exception e) {
-                    currentBuild.result = "FAILURE"
-                    slackSend(channel: config.slack.frontend_channel, message: info)
-                    throw e
+                configFileProvider([configFile(fileId: params.config, variable: 'configFile')]) {
+                    config = readJSON file: configFile
                 }
 
-                info += ":ok_hand:"
-                slackSend(channel: config.slack.frontend_channel, message: info)
+                repository = [
+                    'address': scm.getUserRemoteConfigs()[0].getUrl(),
+                    'source_branch': scm.getBranches()[0].toString(),
+                    'target_branch': scm.getBranches()[0].toString(),
+                    'credentials': config.gitlab.ssh_credentials
+                ]
+
+                repo_name = repository.address.split('/')[4].replace('.git', '')
+                app_name = repo_name.replaceAll("_|\\.", "-")
+                manager.addBadge('orange-square.png', "Version #${params.version}", params.version)
+                info = "*${repo_name}*: Deploy on PROD\n\nVersion: *${params.version}*\nJob URL: ${env.BUILD_URL}\nStatus: "
             }
+
+            try {
+                stage('Clone source') {
+                    try {
+                        gitUtils.checkoutRepository(repository, false)
+                    } catch (checkoutError) {
+                        log.error("Checkout Failed")
+                        info += ":x: checkout faled"
+                        throw checkoutError
+                    }
+
+                    log.info("Checked out branch: ${params.version}")
+                }
+
+                stage('Check image') {
+                    image = "${config.docker_registry.host}/${config.docker_registry.dir}/${repo_name}/${params.package}:${version}"
+                    log.info("Pull image ${image}")
+                    docker.withRegistry("https://${config.docker_registry.host}", config.docker_registry.credentials) {
+                        try {
+                            sh("docker pull ${image}")
+                        } catch (pullError) {
+                            log.error("Failed to pull release image: ${pullError}")
+                            info += ":warning:"
+                            throw pullError
+                        }
+                    }
+                }
+
+                stage('Deploy') {
+                    stand = 'infra'
+                    log.info("Deploy ${image} on PROD")
+                    dir("${WORKSPACE}/${params.package}/devops/k8s") {
+                        try {
+                            withCredentials([file(credentialsId: config.k8s[stand].credentials, variable: 'k8sConfig')]) {
+                                ansiblePlaybook(
+                                    colorized: true,
+                                    playbook: 'playbook.yml',
+                                    extraVars: [
+                                        k8s_config: k8sConfig,
+                                        app_name: app_name,
+                                        package_name: params.package,
+                                        app_version: version,
+                                        image: image,
+                                        stand: stand
+                                    ]
+                                )
+                            }
+                        } catch (deployError) {
+                            log.error("Failed to deploy image: ${deployError}")
+                            info += ":warning:"
+                            throw deployError
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                currentBuild.result = "FAILURE"
+                slackSend(channel: config.slack.frontend_channel, message: info)
+                throw e
+            }
+
+            info += ":ok_hand:"
+            slackSend(channel: config.slack.frontend_channel, message: info)
         }
     }
 }
