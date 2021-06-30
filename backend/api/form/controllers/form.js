b2782eda2c2d9fc5477d7b5003f1bea2e9a17ab0
a19108674 <rritairov@sberbank.ru>
2021-06-30 15:59:20 +0300

    FE-831: Сохранение данных с формы в huntlow

--- backend/api/form/controllers/form.js
+++ backend/api/form/controllers/form.js
@@ -1,4 +1,8 @@
-'use strict';
+const packagejson = require('../../../package.json');
+
+const HUNTFLOW_API = process.env.HUNTFLOW_API || 'https://api.huntflow.ru';
+const HUNTFLOW_TOKEN = process.env.HUNTFLOW_TOKEN || '';
+const HUNTFLOW_ACCOUNT_ID = process.env.HUNTFLOW_ACCOUNT_ID || '';
 
 module.exports = {
   async send(ctx) {
@@ -16,6 +20,67 @@ module.exports = {
         `,
         attachments: [{ filename, path: content }]
       });
+    } catch (err) {
+      console.error(err);
+    }
+
+    try {
+      const formData = new FormData();
+      formData.append('file', content);
+
+      const upload = await fetch(`${HUNTFLOW_API}/account/${HUNTFLOW_ACCOUNT_ID}/upload`, {
+        method: 'POST',
+        body  : formData,
+        headers: {
+          'Authorization': `Bearer ${HUNTFLOW_TOKEN}`,
+          'X-File-Parse': true,
+          'User-Agent': `SberAutoTech/${packagejson.version}`
+        }
+      });
+
+      if(!upload.ok) {
+        throw new Error(upload.statusText);
+      }
+
+      const {
+        id,
+        photo: { id: photo_id },
+        fields: {
+          position,
+          email,
+          salary,
+          name: { middle, last, first },
+          phones,
+          birthdate: { month, day, year }
+        }
+      } = await upload.json();
+
+      const save = await fetch(`${HUNTFLOW_API}/account/${HUNTFLOW_ACCOUNT_ID}/applicants`, {
+        method: 'POST',
+        body  : {
+          last_name: last ?? surname,
+          first_name: first ?? name,
+          middle_name: middle,
+          phone: phones?.[0],
+          email,
+          position: position ?? direction,
+          money: salary,
+          birthday_day: day,
+          birthday_month: month,
+          birthday_year: year,
+          photo: photo_id,
+          externals: [{
+            files: [{ id }]
+          }]
+        },
+        headers: {
+          'Authorization': `Bearer ${HUNTFLOW_TOKEN}`
+        }
+      });
+
+      if(!save.ok) {
+        throw new Error(save.statusText);
+      }
     } catch (err) {
       console.error(err);
     }
