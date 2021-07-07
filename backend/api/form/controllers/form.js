50e5590b9a12b1f87e9c679e795fbf36e07d2f52
a19108674 <rritairov@sberbank.ru>
2021-07-07 15:12:10 +0300

    FE-831: Интеграция huntflow

--- backend/api/form/controllers/form.js
+++ backend/api/form/controllers/form.js
@@ -39,6 +39,12 @@ module.exports = {
     */
 
     try {
+      const me = await req.get('/me');
+
+      console.log('ME: ', me.data);
+
+      ctx.send(me.data);
+
       const buffer = Buffer.from(content.split(',')[1], 'base64');
       const stream = Readable.from(buffer.toString());
       const form = new FormData();
@@ -93,7 +99,6 @@ module.exports = {
 
       console.log(save.data);
 
-      ctx.send();
     } catch (err) {
       console.error(err);
       ctx.send(err);
