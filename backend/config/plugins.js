5adbe77951e68fcbaa2fa3a1e50a83f942b2c98f
Alexander Syrtsov <aasyrtsov@sberbank.ru>
2021-07-09 10:41:02 +0300

    FE-896 Remova S3 integration in development mode

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,29 +1,35 @@
-module.exports = ({ env }) => ({
-  email: {
-    provider: 'nodemailer',
-    providerOptions: {
-      host: env('SMTP_HOST', 'smtp.office365.com'),
-      port: env('SMTP_PORT', 587),
-      auth: {
-        user: env('SMTP_USERNAME'),
-        pass: env('SMTP_PASSWORD'),
+module.exports = ({ env }) => {
+  const config = {}
+  if (env('NODE_ENV') === 'production') {
+    config.upload = {
+      provider: 'aws-s3-enhanced',
+      providerOptions: {
+        endpoint: env('S3_ENDPOINT'),
+        accessKeyId: env('S3_ACCESS_KEY_ID'),
+        secretAccessKey: env('S3_ACCESS_SECRET'),
+        region: env('S3_REGION'),
+        params: {
+          Bucket: env('S3_BUCKET'),
+        },
+        accessLevel: env('ACCESS_LEVEL')
       }
-    },
-    settings: {
-      defaultFrom: 'no-reply-dev@sbauto.tech'
-    },
-  },
-  upload: {
-    provider: 'aws-s3-enhanced',
-    providerOptions: {
-      endpoint: env('S3_ENDPOINT') || 'https://obs.ru-moscow-1.hc.sbercloud.ru',
-      accessKeyId: env('S3_ACCESS_KEY_ID') || 'IAHDXUC6V3E8PQPFO6N3',
-      secretAccessKey: env('S3_ACCESS_SECRET') || 'mQeBl13v4Hj9wAfc7peypn7Rns9HD7r2cIu6gkG2',
-      region: env('S3_REGION') || 'ru-moscow-1',
-      params: {
-        Bucket: env('S3_BUCKET') || 'sberautotech-site-bucket',
+    }
+  };
+  return {
+    ...config,
+    email: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST', 'smtp.office365.com'),
+        port: env('SMTP_PORT', 587),
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+        }
       },
-      accessLevel: env('ACCESS_LEVEL') || 'public-read'
-    },
+      settings: {
+        defaultFrom: 'no-reply-dev@sbauto.tech'
+      },
+    }
   }
-});
+};
