3c13efd491572c2b1fbb2f9db0e024fff1bbbc2f
ArtyomResh <artyom.resh@csssr.com>
2022-10-20 16:19:54 +0800

    SBER-10: Верстка блока "Правила для пассажиров"

--- frontend/src/pages/public-beta-signup.tsx
+++ frontend/src/pages/public-beta-signup.tsx
@@ -13,6 +13,15 @@ const features = [
     'Вы&#160;можете записаться на&#160;тестирование только если вам уже исполнилось 18&#160;лет, а&#160;ваш +1&#160;может быть любого возраста'
 ];
 
+const rules = [
+    { imageName: 'two-passengers', text: 'Два пассажирских<br />места', alt: 'Изображение знака с двумя людьми пристёгнутыми ремнём безопасности' },
+    { imageName: 'no-pets', text: 'Без питомцев', alt: 'Изображение знака с перечёркнутой лапки животного' },
+    { imageName: 'no-photo-inside', text: 'Фото только<br />снаружи', alt: 'Изображение знака с внешним видом автомобиля' },
+    { imageName: 'no-drinks', text: 'Без еды<br />и&#160;напитков', alt: 'Изображение знака с перечёркнутым стаканом' },
+    { imageName: 'buckle-up', text: 'Пристегнуть<br />ремни', alt: 'Изображения знака с человеком пристёгнутым ремнём безопасности' },
+    { imageName: 'no-smoking', text: 'Не&#160;курить', alt: 'Изображения знака с перечёркнутой сигаретой' }
+];
+
 const PublicBetaSignup = () => {
     const cn = useClassnames(style);
 
@@ -38,6 +47,37 @@ const PublicBetaSignup = () => {
                         <Card key={index} className={cn('public-beta-signup__features-list-item')} as="li">
                             {formatText(feature)}
                         </Card>
+                    ))}
+                </ul>
+            </section>
+
+            <section className={cn('public-beta-signup__section-wrapper')}>
+                <Heading
+                    className={cn('public-beta-signup__rules-title')}
+                    level={2}
+                    dangerouslySetInnerHTML={{ __html: formatText('А&#160;вот простые {правила для пассажиров}') }}
+                />
+
+                <ul className={cn('public-beta-signup__rules-list')}>
+                    {rules.map((rule, index) => (
+                        <li key={index} className={cn('public-beta-signup__rules-list-item')}>
+                            <figure className={cn('public-beta-signup__rule')}>
+                                <img
+                                    className={cn('public-beta-signup__rule-image')}
+                                    srcSet={`/rules/${rule.imageName}/${rule.imageName}@1x.png 1x, /rules/${rule.imageName}/${rule.imageName}@2x.png 2x`}
+                                    src={`/rules/${rule.imageName}/${rule.imageName}@3x.png`}
+                                    alt={rule.alt}
+                                />
+                                <Text
+                                    className={cn('public-beta-signup__rule-caption')}
+                                    size={4}
+                                    as="figcaption"
+                                    dangerouslySetInnerHTML={{ __html: formatText(rule.text) }}
+                                />
+
+                            </figure>
+                        </li>
+
                     ))}
                 </ul>
 
