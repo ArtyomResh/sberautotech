1fe88b7f2bcc5090c70f86114800c1b9409e725e
ArtyomResh <artyom.resh@csssr.com>
2022-11-04 16:17:43 +0800

    SBER-7: Верстка блока с картой
    - что бы исправить замечания по SBER-7, пришлось полностью переписывать вёрстку первого блока страницы
    - бонусом поправил так же вёрстку по SBER-6
    - перегруппировал компоненты по фолдерам
    - добавил 3x изображения карт

---
+++ frontend/src/components/public-beta-signup/components/hero/index.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+
+import IconArrowRight from '../../../../../static/hero/icon-arrow-right.inline.svg';
+import IconArrowDown from '../../../../../static/hero/icon-arrow-down.inline.svg';
+import { useClassnames } from '../../../../hooks/use-classnames';
+import { formatText } from '../../../../utils';
+
+import LinkButton from '../../../link-button';
+import Heading from '../../../heading';
+import Text from '../../../text';
+import GridWrapper from '../grid-wrapper';
+
+import styles from './index.css';
+
+
+const OpenTesting = () => {
+    const cn = useClassnames(styles);
+
+    return (
+        <section className={cn('hero')}>
+            <GridWrapper as="div">
+                <Heading
+                    level={1}
+                    className={cn('hero__title')}
+                    dangerouslySetInnerHTML={{ __html: formatText('{Беспилотные автомобили} ждут первых пассажиров') }}
+                />
+
+                <Heading level={2} className={cn('hero__announce')} as="p">
+                    Уже в ноябре!
+                </Heading>
+
+                <LinkButton
+                    className={cn('hero__button')}
+                    href="/public-beta-signup#modal"
+                >
+                    Принять участие
+                </LinkButton>
+            </GridWrapper>
+
+            <div className={cn('hero__map')} />
+
+            <GridWrapper as="div" className={cn('hero__location')}>
+                <Text className={cn('hero__location-details-title')} size={4} as="h3">
+                    Локация
+                </Text>
+                <Heading level={2} as="p" className={cn('hero__location-details')}>
+                    Крылатское
+                    <IconArrowRight className={cn('hero__location-details-icon_arrow-right')} />
+                </Heading>
+
+                <Text className={cn('hero__location-details-title')} size={4} as="h3">
+                    Протяженность маршрута
+                </Text>
+                <Heading level={2} as="p" className={cn('hero__location-details')}>
+                    30&#160;км
+                </Heading>
+
+                <Text className={cn('hero__location-details-title')} size={4} as="h3">
+                    Машин на&#160;линии
+                </Text>
+                <Heading level={2} as="p" className={cn('hero__location-details')}>
+                    10
+                </Heading>
+
+                <IconArrowDown className={cn('hero__location-icon_arrow-down')} />
+            </GridWrapper>
+        </section>
+    );
+};
+
+export default OpenTesting;
--- frontend/src/components/public-beta-signup/index.tsx
+++ frontend/src/components/public-beta-signup/index.tsx
@@ -2,16 +2,19 @@ import React from 'react';
 
 import Layout from '../layout';
 
-import OpenTesting from '../beta-test-signup/open-beta';
 import FeaturesSection from './components/features-section';
+import PathDescription from './components/path-description';
 import Footer from './components/footer';
+import Hero from './components/hero';
 import VideoSection from './components/video-section';
 import RulesSection from './components/rules-section';
 import Alert from './components/alert';
 
 const PublicBetaSignup = () => (
     <Layout theme={{ mode: 'dark', logoColor: '#040A0A' }}>
-        <OpenTesting testingStartsAt={new Date()} />
+        <Hero />
+
+        <PathDescription />
 
         <FeaturesSection />
 
