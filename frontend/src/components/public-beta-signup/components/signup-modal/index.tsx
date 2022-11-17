acb6a55f37c0b22e2a04f26b3c06a8cf60f9b571
Evgeny Chirko <zhenya_chirko@mail.ru>
2022-11-17 17:14:19 +0300

    SBER-56: Форма записи Safari. Не происходит открытия формы записи при клике на кнопку Принять участие
    Выносим HOC компонент для решения проблемы регидрации

--- frontend/src/components/public-beta-signup/components/signup-modal/index.tsx
+++ frontend/src/components/public-beta-signup/components/signup-modal/index.tsx
@@ -20,7 +20,7 @@ import Text from '../../../text';
 
 import styles from './index.css';
 import selectStyles from './select.styles';
-import { useIsClient } from '../../../../utils/hooks/useIsClient';
+import { UpdateOnMount } from '../../../update-on-mount';
 
 interface IMobileOption {
     value: 'ios' | 'android', label: string
@@ -83,7 +83,6 @@ const CustomValueContainer = (props: SelectProps['ValueContainer']) => {
 
 const SignupModal = (props: IProps) => {
     const location = useLocation();
-    const isClient = useIsClient();
     const cn = useClassnames(styles);
     const methods = useForm<ISignupFormData>({
         mode: 'onSubmit'
@@ -146,122 +145,124 @@ const SignupModal = (props: IProps) => {
     );
 
     return (
-        <div className={cn('signup-modal__overlay', { 'signup-modal__overlay_visible': isVisible })} key={String(isClient)}>
-            <div className={cn('signup-modal')}>
-                <Link onClick={closeModal} to="/public-beta-signup" className={cn('signup-modal__button-close')}>
-                    <CrossIcon />
-                </Link>
+        <UpdateOnMount>
+            <div className={cn('signup-modal__overlay', { 'signup-modal__overlay_visible': isVisible })}>
+                <div className={cn('signup-modal')}>
+                    <Link onClick={closeModal} to="/public-beta-signup" className={cn('signup-modal__button-close')}>
+                        <CrossIcon />
+                    </Link>
 
-                <div className={cn('signup-modal__description')}>
-                    <Heading
-                        className={cn('signup-modal__description-title')}
-                        level={1}
-                        as="h2"
-                        dangerouslySetInnerHTML={{ __html: 'Заполните информацию о&nbsp;себе, мы&nbsp;скоро вернемся.' }}
-                    />
+                    <div className={cn('signup-modal__description')}>
+                        <Heading
+                            className={cn('signup-modal__description-title')}
+                            level={1}
+                            as="h2"
+                            dangerouslySetInnerHTML={{ __html: 'Заполните информацию о&nbsp;себе, мы&nbsp;скоро вернемся.' }}
+                        />
 
-                    <Text size={4} as="p" className={cn('signup-modal__description-paragraph')}>
-                        В&nbsp;рамках тестирования мы&nbsp;проводим несколько поездок ежедневно, поэтому будем приглашать участников
-                        небольшими группами.
-                        <br /><br />
-                        Когда вы&nbsp;попадете в&nbsp;тестовую группу, вам на&nbsp;почту придет ссылка на&nbsp;приложение. Не&nbsp;забудьте проверить
-                        папку &laquo;Спам&raquo;!
-                        <br /><br />
-                        В&nbsp;приложении есть возможность выбрать точки&nbsp;А и&nbsp;Б на&nbsp;маршруте&nbsp;&mdash; заберем в&nbsp;нужном месте и&nbsp;отвезем,
-                        куда захотите.
-                    </Text>
+                        <Text size={4} as="p" className={cn('signup-modal__description-paragraph')}>
+                            В&nbsp;рамках тестирования мы&nbsp;проводим несколько поездок ежедневно, поэтому будем приглашать участников
+                            небольшими группами.
+                            <br /><br />
+                            Когда вы&nbsp;попадете в&nbsp;тестовую группу, вам на&nbsp;почту придет ссылка на&nbsp;приложение. Не&nbsp;забудьте проверить
+                            папку &laquo;Спам&raquo;!
+                            <br /><br />
+                            В&nbsp;приложении есть возможность выбрать точки&nbsp;А и&nbsp;Б на&nbsp;маршруте&nbsp;&mdash; заберем в&nbsp;нужном месте и&nbsp;отвезем,
+                            куда захотите.
+                        </Text>
+                    </div>
+
+                    <FormProvider {...methods}>
+                        <form onSubmit={methods.handleSubmit<ISignupFormData>(handleSubmit)} className={cn('signup-modal__form')}>
+                            <Input
+                                className={cn('signup-modal__form-field', 'signup-modal__form-field_full-width')}
+                                type="text"
+                                placeholder="Как вас зовут?"
+                                name={formFields.name}
+                                requiredValidation={true}
+                            />
+                            <Input
+                                className={cn('signup-modal__form-field')}
+                                type="email"
+                                placeholder="Электронная почта"
+                                name={formFields.email}
+                                requiredValidation={true}
+                            />
+
+                            <Input
+                                className={cn('signup-modal__form-field')}
+                                type="tel"
+                                placeholder="Номер телефона"
+                                name={formFields.phoneNumber}
+                                requiredValidation={true}
+                            />
+
+
+                            <div className={cn('signup-modal__form-field')}>
+                                <Select
+                                    options={districtOptions}
+                                    placeholder="В каком районе вы живете?"
+                                    name={formFields.district}
+                                    required={true}
+                                    styles={selectStyles}
+                                    components={{
+                                        ValueContainer: CustomValueContainer
+                                    }}
+                                />
+                            </div>
+                            <div className={cn('signup-modal__form-field')}>
+                                <Select
+                                    options={mobileOsOptions}
+                                    placeholder="ОС на вашем телефоне"
+                                    name={formFields.mobileOS}
+                                    styles={selectStyles}
+                                    required={true}
+                                    components={{
+                                        ValueContainer: CustomValueContainer
+                                    }}
+                                />
+                            </div>
+
+                            <CheckBox
+                                className={cn(
+                                    'signup-modal__form-field',
+                                    'signup-modal__form-field_checkbox',
+                                    'signup-modal__form-field_full-width'
+                                )}
+                                label={(
+                                    <Text size={4} as="p">
+                                        Мне исполнилось 18&#160;лет
+                                    </Text>
+                                )}
+                                name={formFields.eighteen}
+                                isBlock={true}
+                                requiredValidation={true}
+                            />
+
+                            <CheckBox
+                                className={cn(
+                                    'signup-modal__form-field',
+                                    'signup-modal__form-field_checkbox',
+                                    'signup-modal__form-field_full-width'
+                                )}
+                                label={(
+                                    <Text size={4} as="p">
+                                        Даю согласие на&#160;обработку моих персональных данных в&#160;соответствии с&#160;политикой конфиденциальности и&#160;на&#160;участие в&#160;эксперименте
+                                    </Text>
+                                )}
+                                name={formFields.policy}
+                                isBlock={true}
+                                requiredValidation={true}
+                            />
+
+                            <Button type="submit" className={cn('signup-modal__submit')} isBlock={true} isLoading={isSubmitting}>
+                                Отправить
+                            </Button>
+                        </form>
+                    </FormProvider>
                 </div>
-
-                <FormProvider {...methods}>
-                    <form onSubmit={methods.handleSubmit<ISignupFormData>(handleSubmit)} className={cn('signup-modal__form')}>
-                        <Input
-                            className={cn('signup-modal__form-field', 'signup-modal__form-field_full-width')}
-                            type="text"
-                            placeholder="Как вас зовут?"
-                            name={formFields.name}
-                            requiredValidation={true}
-                        />
-                        <Input
-                            className={cn('signup-modal__form-field')}
-                            type="email"
-                            placeholder="Электронная почта"
-                            name={formFields.email}
-                            requiredValidation={true}
-                        />
-
-                        <Input
-                            className={cn('signup-modal__form-field')}
-                            type="tel"
-                            placeholder="Номер телефона"
-                            name={formFields.phoneNumber}
-                            requiredValidation={true}
-                        />
-
-
-                        <div className={cn('signup-modal__form-field')}>
-                            <Select
-                                options={districtOptions}
-                                placeholder="В каком районе вы живете?"
-                                name={formFields.district}
-                                required={true}
-                                styles={selectStyles}
-                                components={{
-                                    ValueContainer: CustomValueContainer
-                                }}
-                            />
-                        </div>
-                        <div className={cn('signup-modal__form-field')}>
-                            <Select
-                                options={mobileOsOptions}
-                                placeholder="ОС на вашем телефоне"
-                                name={formFields.mobileOS}
-                                styles={selectStyles}
-                                required={true}
-                                components={{
-                                    ValueContainer: CustomValueContainer
-                                }}
-                            />
-                        </div>
-
-                        <CheckBox
-                            className={cn(
-                                'signup-modal__form-field',
-                                'signup-modal__form-field_checkbox',
-                                'signup-modal__form-field_full-width'
-                            )}
-                            label={(
-                                <Text size={4} as="p">
-                                    Мне исполнилось 18&#160;лет
-                                </Text>
-                            )}
-                            name={formFields.eighteen}
-                            isBlock={true}
-                            requiredValidation={true}
-                        />
-
-                        <CheckBox
-                            className={cn(
-                                'signup-modal__form-field',
-                                'signup-modal__form-field_checkbox',
-                                'signup-modal__form-field_full-width'
-                            )}
-                            label={(
-                                <Text size={4} as="p">
-                                    Даю согласие на&#160;обработку моих персональных данных в&#160;соответствии с&#160;политикой конфиденциальности и&#160;на&#160;участие в&#160;эксперименте
-                                </Text>
-                            )}
-                            name={formFields.policy}
-                            isBlock={true}
-                            requiredValidation={true}
-                        />
-
-                        <Button type="submit" className={cn('signup-modal__submit')} isBlock={true} isLoading={isSubmitting}>
-                            Отправить
-                        </Button>
-                    </form>
-                </FormProvider>
             </div>
-        </div>
+        </UpdateOnMount>
     );
 };
 
