a42537d5e483b7f9e8d668d4f8b724df069a6b3f
Evgeny Chirko <zhenya_chirko@mail.ru>
2022-11-22 17:05:09 +0300

    SBER-74: Запись на бета-тест. Обводка чекбокса остается красной при выборе

--- frontend/src/components/respond-form/check-box/index.tsx
+++ frontend/src/components/respond-form/check-box/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import { ValidationRule } from 'react-hook-form/dist/types/validator';
 import { Message } from 'react-hook-form/dist/types/form';
 
@@ -16,11 +16,21 @@ export interface IRadioButtonProps {
     className?: string,
     requiredValidation?: Message | ValidationRule<boolean>,
     isBlock?: boolean,
-    onChange?: (e: InputEvent) => void
+    onChange?: (e: ChangeEvent<HTMLInputElement>) => void
 }
 
 const CheckBox: React.FC<IRadioButtonProps> = ({ label, name, isBlock, ...props }: IRadioButtonProps) => {
     const { register, formState } = useFormContext();
+
+
+    const { onChange, ...registerProps } = register(name, {
+        required: props.requiredValidation
+    });
+
+    const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+        void onChange(e);
+        props.onChange?.(e);
+    };
 
     const cn = useClassnames(style);
 
@@ -29,10 +39,8 @@ const CheckBox: React.FC<IRadioButtonProps> = ({ label, name, isBlock, ...props 
             <input
                 type="checkbox"
                 className={cn('check-box__input', { 'check-box__input_error': formState.errors?.[name] })}
-                {...register(name, {
-                    required: props.requiredValidation
-                })}
-                onChange={props?.onChange}
+                {...registerProps}
+                onChange={handleChange}
             />
             <label className={cn('check-box__label', { 'check-box__label_block': isBlock })}>
                 {label}
