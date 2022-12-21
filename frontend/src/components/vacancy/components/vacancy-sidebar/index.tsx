224684911aade55068125e31f1a54e150406b10c
ArtyomResh <artyom.resh@csssr.com>
2022-12-21 22:12:24 +0800

    SBER-143: Убрать иконки соцсетей со страницы вакансий

--- frontend/src/components/vacancy/components/vacancy-sidebar/index.tsx
+++ frontend/src/components/vacancy/components/vacancy-sidebar/index.tsx
@@ -1,38 +1,20 @@
-import React, { useCallback, useMemo, useState } from 'react';
+import React from 'react';
 
 import { useClassnames } from '../../../../hooks/use-classnames';
-
-import Tooltip from '../../../tooltip';
-import ButtonWrapper from '../button-wrapper';
-
-import { IStrapiVacancies } from '../../../../pages/vacancy';
-
 import ArrowLeft from '../../../../images/arrow-left.inline.svg';
-import FaceBookIcon from '../../../../images/facebook-icon.inline.svg';
-import TwitterIcon from '../../../../images/twitter-icon.inline.svg';
-import VKIcon from '../../../../images/vk-icon.inline.svg';
-import ShareLinkIcon from '../../../../images/share-link-icon.inline.svg';
+
+import { IStrapiVacancies } from '../..';
+
+import ButtonWrapper from '../button-wrapper';
 
 import styles from './index.css';
 
-interface IProps {
+interface IProps extends Pick<IStrapiVacancies, 'city' | 'jobType' | 'title' | 'huntflowId'> {
     backToPreviousPage: () => void
 }
 
-const VacancySidebar = ({ city, jobType, backToPreviousPage, title, huntflowId }: IStrapiVacancies & IProps) => {
+const VacancySidebar = ({ city, jobType, title, huntflowId, backToPreviousPage }: IProps) => {
     const cn = useClassnames(styles);
-
-    const [tooltipIsOpen, setTooltipIsOpen] = useState(false);
-
-    const urlHref = useMemo(() => (typeof window !== 'undefined' ? window.location.href : ''), []);
-
-    const URLCopier = useCallback(() => {
-        void navigator.clipboard.writeText(urlHref);
-        setTooltipIsOpen(true);
-        setTimeout(() => {
-            setTooltipIsOpen(false);
-        }, 1000);
-    }, []);
 
     return (
         <div className={cn('vacancy-sidebar')}>
@@ -62,17 +44,6 @@ const VacancySidebar = ({ city, jobType, backToPreviousPage, title, huntflowId }
                         Откликнуться
                     </ButtonWrapper>
                 </div>
-            </div>
-
-            <div className={cn('vacancy-sidebar__bottom')}>
-                <div className={cn('vacancy-sidebar__links-wrapper')}>
-                    <a target="_blank" href={`http://www.facebook.com/share.php?u=${urlHref}`}><FaceBookIcon /></a>
-                    <a target="_blank" href={`http://twitter.com/intent/tweet?status=${urlHref}`}><TwitterIcon /></a>
-                    <a target="_blank" href={`http://vk.com/share.php?url=${urlHref}`}><VKIcon /></a>
-                    <a onClick={URLCopier} ><ShareLinkIcon /></a>
-                </div>
-
-                {tooltipIsOpen ? <Tooltip label="Скопировано" /> : null}
             </div>
         </div>
     );
